a078a99d39d8385a76b6dc4e560a9b8d381d2b74
Inktomi93 <94721064+Inktomi93@users.noreply.github.com>
2025-12-26 11:25:37 -0700

    feat: add CI workflow for automated builds and testing

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -1,11 +1,16 @@
-/* eslint-env node */
 import { readFileSync, writeFileSync } from 'fs';
 
 const manifest = JSON.parse(readFileSync('manifest.json', 'utf8'));
 const version = manifest.version;
 
+// Sync to constants.ts
 let constants = readFileSync('src/constants.ts', 'utf8');
 constants = constants.replace(/VERSION = '[^']+'/, `VERSION = '${version}'`);
 writeFileSync('src/constants.ts', constants);
+
+// Sync to package.json
+const pkg = JSON.parse(readFileSync('package.json', 'utf8'));
+pkg.version = version;
+writeFileSync('package.json', JSON.stringify(pkg, null, 2) + '\n');
 
 console.log(`Synced version: ${version}`);
